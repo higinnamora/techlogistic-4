3a2ef66fed1ba3bd7f4a492afd61ec61f5b6660c
Cesar <cesar_a_mar_@hotmail.com>
2023-09-16 17:23:12 -0500

    actualizacion del procedimiento almacenado para consultar funcionarios

--- dashboard/consultarFuncionarios.php
+++ dashboard/consultarFuncionarios.php
@@ -10,15 +10,46 @@
         if ($stmt->execute()) {
             $result = $stmt->get_result();
 
+            echo '<table>
+                <tr> 
+                    <th>Id Persona</th>
+                    <th>No Documento</th>
+                    <th>Primer Nombre</th>
+                    <th>Segundo Nombre</th>
+                    <th>Primer Apellido</th>
+                    <th>Segundo Apellido</th>
+                </tr>';
+
             while ($fila = $result->fetch_assoc()) {
-                echo "ID: " . $fila['id_persona'] . "<br>";
+                /*echo "ID: " . $fila['id_persona'] . "<br>";
                 echo "Numero de documento: " . $fila['no_documento'] . "<br>";
                 echo "Nombres: " . $fila['primer_nombre'] . " " . $fila['segundo_nombre'] . "<br>";
                 echo "Apellidos: " . $fila['primer_apellido'] . " " . $fila['segundo_apellido'] . "<br>";
                 echo "Correo: " . $fila['correo'] . "<br>";
                 echo "Rol: " . $fila['rol'] . "<br>";
-                echo "<br>\n\n";
+                echo "<br>\n\n";*/
+
+                $id = $fila["id_persona"];
+                $ndoc = $fila["no_documento"];
+                $PrimerNombr = $fila["primer_nombre"];
+                $SegundoNombr = $fila["segundo_nombre"];
+                $primerApell = $fila["primer_apellido"];
+                $segundoApell = $fila["segundo_apellido"];
+                $correo = $fila["correo"];
+                $rol = $fila["rol"];
+            echo "
+                <tr>
+                    <td>$id</td>
+                    <td>$ndoc</td>
+                    <td>$PrimerNombr</td>
+                    <td>$SegundoNombr</td>
+                    <td>$primerApell</td> 
+                    <td>$segundoApell</td>
+                    <td>$correo</td>
+                    <td>$rol</td>
+                </tr>";
             }
+            echo "</table>";
             $stmt->close();
         } else {
             die("Error en la ejecución del procedimiento almacenado: " . $stmt->error);
