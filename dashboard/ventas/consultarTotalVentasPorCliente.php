071b055286e83901fc21190933ea901a79a1d729
Cesar <cesar_a_mar_@hotmail.com>
2023-09-16 18:33:12 -0500

    Creacion procedmiento almacenado ConsultarTotalVentasClientes()

---
+++ dashboard/ventas/consultarTotalVentasPorCliente.php
@@ -0,0 +1,45 @@
+<?php
+
+    $conn = new mysqli('localhost', 'root', 'admin', 'techlogistic');
+    /* 
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $consultar = $_POST["consultardash"];
+    }*/
+ 
+    $sql = "CALL ConsultarTotalVentasClientes()";
+    if ($stmt = $conn->prepare($sql)) {
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+
+            echo '<table>
+                <tr> 
+                    <th>Id Persona</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Total Ventas</th>
+                </tr>';
+
+            while ($fila = $result->fetch_assoc()) {
+
+                $id = $fila["id_persona"];
+                $nombre = $fila["primer_nombre"];
+                $apellido = $fila["primer_apellido"];
+                $totalVentas = $fila["total_ventas"];
+            echo "
+                <tr>
+                    <td>$id</td>
+                    <td>$nombre</td>
+                    <td>$apellido</td>
+                    <td>$totalVentas</td>
+                </tr>";
+            }
+            echo "</table>";
+            $stmt->close();
+        } else {
+            die("Error en la ejecución del procedimiento almacenado: " . $stmt->error);
+        }
+    } else {
+        die("Error en la preparación del procedimiento almacenado: " . $conex->error);
+    }
+    mysqli_close($conn);
+?>
