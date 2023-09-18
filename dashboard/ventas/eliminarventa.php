9a85e01f067b46ed1ea4b522ebd7c0a34221ae6f
Juan Hernandez <informatica64jcds@gmail.com>
2023-09-18 16:03:45 -0500

    se agrega el boton de eliminar en el modulo de ventas y se le da funcionalidad

---
+++ dashboard/ventas/eliminarventa.php
@@ -0,0 +1,35 @@
+<?php
+
+$conexion;
+include_once "../../conexion_a_la_DB.php";
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $eliminarven = $_POST["eliminar_venta"];
+}
+
+$eliminarven = (int)$eliminarven;
+
+$sql = "DELETE FROM orden_venta_producto WHERE numero_orden_venta = '$eliminarven';";
+$sql2 = "DELETE FROM orden_venta WHERE numero_orden_venta = '$eliminarven';";
+if ($conexion->query($sql) === TRUE) {
+    echo "Elemento eliminado correctamente";
+} else {
+    echo "Error al eliminar el elemento: " . $conexion->error;
+}
+if ($conexion->query($sql2) === TRUE) {
+    echo "Elemento eliminado correctamente";
+} else {
+    echo "Error al eliminar el elemento: " . $conexion->error;
+}
+
+
+
+
+
+$conexion->close();
+
+
+
+
+?>
