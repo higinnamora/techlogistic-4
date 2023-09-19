b81774a3a3252c78895e2a2492c31aa73e175d90
Cesar <cesar_a_mar_@hotmail.com>
2023-09-18 23:21:45 -0500

    Actualizacion

--- conexion_a_la_DB.php
+++ conexion_a_la_DB.php
@@ -2,23 +2,8 @@
 
     $user = "root";
     $server = "localhost";
-<<<<<<< HEAD
     $password = "admin";
     $db = "techlogistic";
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    $password = "admin";
-    $db = "techlogistic";
-=======
-    $password = "Aura2117*";//Cambiar clave
-    $db = "techlogisticdb";
->>>>>>> Stashed changes
-=======
-    $password = "";
-    $db = "techlogisticdb";
->>>>>>> Stashed changes
->>>>>>> 2ed32f938e1c8d37ecedc97ef61bb3020f704115
     $conexion = new mysqli($server, $user, $password, $db);
     
 if (!$conexion) {
