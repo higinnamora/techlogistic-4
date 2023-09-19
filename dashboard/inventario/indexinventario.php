ce8f6196d193a7df054a0ceafb8298837ac5ec26
higinnamora <higinnamora@gmail.com>
2023-09-19 15:42:26 -0500

    Techlogistic: nuevo producto page styles

--- dashboard/inventario/indexinventario.php
+++ dashboard/inventario/indexinventario.php
@@ -27,7 +27,8 @@ $datos = $conexion->query($sql);
   <!-- Box icons-->
   <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   <!-- Bootstrap 5 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- Estilos CSS -->
   <link rel="stylesheet" href="../../styles/techlogistic.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
@@ -38,8 +39,10 @@ $datos = $conexion->query($sql);
   <!-- Header -->
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a href="../../indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../favicon.png" alt="" class="navigation__image">Techlogistic</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <a href="../../indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../favicon.png" alt=""
+          class="navigation__image">Techlogistic</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -52,7 +55,8 @@ $datos = $conexion->query($sql);
           <li class="nav-item dropdown">
             <div class="dropdown" role="group">
               <a class="dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://higinnamora.github.io/techlogistic/images/profile/profile.png" alt="mdo" class="rounded-circle" width="38" height="38" />
+                <img src="https://higinnamora.github.io/techlogistic/images/profile/profile.png" alt="mdo"
+                  class="rounded-circle" width="38" height="38" />
               </a>
               <ul class="dropdown-menu dropdown-menu-lg-end">
                 <!--<li><a class="dropdown-item" href="../../502.html" target="_blank">Mi perfil</a></li>
@@ -77,7 +81,7 @@ $datos = $conexion->query($sql);
         <a href="./nuevo-producto.html"> <button class="btn btn-primary w-auto">Agregar producto</button></a>
       </div>
     </div>
-    <hr>   
+    <hr>
 
 
     <div class="table-responsive">
@@ -100,13 +104,14 @@ $datos = $conexion->query($sql);
               echo "<td>$valor</td>";
             }
             ?>
-              <td>
-                <div class="dropdown">
-                  <button class="btn btn-sm btn-outline-secondary border-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class='bx bx-dots-horizontal-rounded'></i>
-                  </button>
-                </div>
-              </td>
+            <td>
+              <div class="dropdown">
+                <button class="btn btn-sm btn-outline-secondary border-0" type="button" id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class='bx bx-dots-horizontal-rounded'></i>
+                </button>
+              </div>
+            </td>
             <?php
             echo "</tr>";
           }
@@ -115,73 +120,58 @@ $datos = $conexion->query($sql);
       </table>
     </div>
     <h4>Actualizar Inventario</h4>
-    
-<form class="form" id="sign-up-form"  action="actualizardash.php" method="POST">
 
-            <div class="form-field">
-              <label for="cantidad">Codigo stock</label>
-              <input
-                type="number"
-                placeholder="ingrese codigo"
-                id="codigo"
-                name="codigo"
-                required
-              />
-            </div>
-          
+    <form class="form" id="sign-up-form" action="actualizardash.php" method="POST">
+
+      <div class="form-field">
+        <label for="cantidad">Codigo stock</label>
+        <input type="number" placeholder="ingrese codigo" id="codigo" name="codigo" required />
+      </div>
 
 
-            <div class="form-field">
-              <label for="cantidad">Cantidad en stock</label>
-              <input
-                type="number"
-                placeholder="ingrese cantidad de producto disponible"
-                id="cantidad"
-                name="cantidad"
-                required
-              />
-            </div>
 
-            <div class="form-field">
-              <label for="descripcion">Descripcion</label>
-              <input
-                type="text"
-                placeholder="Nombre del producto"
-                id="descripcion"
-                name="descripcion"
-                required
-              />
-            </div>
+      <div class="form-field">
+        <label for="cantidad">Cantidad en stock</label>
+        <input type="number" placeholder="ingrese cantidad de producto disponible" id="cantidad" name="cantidad"
+          required />
+      </div>
 
-            <div class="mb-3">
+      <div class="form-field">
+        <label for="descripcion">Descripcion</label>
+        <input type="text" placeholder="Nombre del producto" id="descripcion" name="descripcion" required />
+      </div>
+
+      <div class="mb-3">
         <label for="estadoproducto">Disponibilidad del producto</label>
-        <select  name="estadoproducto">
+        <select name="estadoproducto">
           <option value="Disponible">Disponible</option>
           <option value="Agotado">Agotado</option>
         </select>
       </div>
-          <input class="button" type="submit" value="Actualizar" />
-          </form>
+      <input class="button" type="submit" value="Actualizar" />
+    </form>
 
     <h4>Eliminar orden de venta</h4>
 
-                <form class="newsletter-form" action="eliminardash.php" id="newsletter-form" method="POST">
-                <div class="form-field">
-                  <input type="number" name="eliminar_inventario" placeholder="codigo del stock " class="newsletter-input"
-                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" required>
-                  <button class="button" type="submit">Eliminar</button>
-                </form>
+    <form class="newsletter-form" action="eliminardash.php" id="newsletter-form" method="POST">
+      <div class="form-field">
+        <input type="number" name="eliminar_inventario" placeholder="codigo del stock " class="newsletter-input"
+          pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" required>
+        <button class="button" type="submit">Eliminar</button>
+    </form>
   </main>
   <div class="copyright">
-      <div class="bd-container">
-        <p>💙 © 2023 Techlogistic. Todos los derechos reservados. 💚</p>
-        <p><a href="./../../terminos-y-condiciones.html">Términos y Condiciones</a> · <a
-            href="../../politica-de-privacidad.html">Política de Privacidad</a></p>
-      </div>
+    <div class="bd-container">
+      <p>💙 © 2023 Techlogistic. Todos los derechos reservados. 💚</p>
+      <p><a href="./../../terminos-y-condiciones.html">Términos y Condiciones</a> · <a
+          href="../../politica-de-privacidad.html">Política de Privacidad</a></p>
     </div>
+  </div>
   </footer>
 
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -190,7 +180,7 @@ $datos = $conexion->query($sql);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
 <script>
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('#stock').DataTable({
       dom: 'Bfrtip',
       buttons: [
