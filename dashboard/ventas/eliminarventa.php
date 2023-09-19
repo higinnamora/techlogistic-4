992327c60921e7a46d64041293f43a62ef6d5179
higinnamora <higinnamora@gmail.com>
2023-09-19 18:00:56 -0500

    Techlogistic: page success: eliminar venta

--- dashboard/ventas/eliminarventa.php
+++ dashboard/ventas/eliminarventa.php
@@ -3,21 +3,21 @@
 $conexion;
 include_once "../../conexion_a_la_DB.php";
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $eliminarven = $_POST["eliminar_venta"];
 }
 
-$eliminarven = (int)$eliminarven;
+$eliminarven = (int) $eliminarven;
 
 $sql = "DELETE FROM orden_venta_producto WHERE numero_orden_venta = '$eliminarven';";
 $sql2 = "DELETE FROM orden_venta WHERE numero_orden_venta = '$eliminarven';";
 if ($conexion->query($sql) === TRUE) {
-    echo "Elemento eliminado correctamente";
+    header("Location: /dashboard/ventas/eliminar-venta-exitosa.php");
 } else {
     echo "Error al eliminar el elemento: " . $conexion->error;
 }
 if ($conexion->query($sql2) === TRUE) {
-    echo "Elemento eliminado correctamente";
+    header("Location: /dashboard/ventas/eliminar-venta-exitosa.php");
 } else {
     echo "Error al eliminar el elemento: " . $conexion->error;
 }
