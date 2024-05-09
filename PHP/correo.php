b1dd2036eb7feaa64fddf79c223cd51957d05a4d
aura <auravelagonzalez@gmail.com>
2024-05-08 20:38:09 -0500

    Se modifica formulario de correo

--- PHP/correo.php
+++ PHP/correo.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -50,16 +51,13 @@
       }
       //mysqli_close($conn);
       ?>
-<form class="formulario" action="" method="POS">
-                <fieldset>
-                  <legend>Contacto</legend>
-
-                  <label for="email">E-mail</label>
-                  <input type="email" placeholder="Email de recepción" id="email">
-
-                  <label for="mensaje">Mensaje</label>
-                  <textarea type="mensaje" name="Tu mensaje"></textarea>
-                </fieldset>
-              </form>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
 </body>
 </html>
