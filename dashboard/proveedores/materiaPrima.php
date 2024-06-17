427db77d85e838e0b1c00c101357a08f25f730fe
Ginna Mora <higinnamora@gmail.com>
2024-06-17 11:29:45 -0500

    Techlogistic: Update of supplier module actions.

--- dashboard/proveedores/materiaPrima.php
+++ dashboard/proveedores/materiaPrima.php
@@ -2,42 +2,36 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Techlogistic</title>
-    <meta name="description" content="">
-    <!-- Favicon -->
-    <link rel="icon" href="../../images/favicon.png">
-    <!-- Box icons-->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <!-- Estilos CSS -->
-    <link rel="stylesheet" href="../../HTML/styles/techlogistic.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Techlogistic</title>
+  <meta name="description" content="">
+  <!-- Favicon -->
+  <link rel="icon" href="../../images/favicon.png">
+  <!-- Box icons-->
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <!-- Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <!-- Estilos CSS -->
+  <link rel="stylesheet" href="../../HTML/styles/techlogistic.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
 </head>
 
 <body class="vh-100">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../IMAGES/favicon.png" alt="" class="navigation__image">Techlogistic</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../IMAGES/favicon.png"
+          alt="" class="navigation__image">Techlogistic</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto me-4 mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="../../PHP/indexdash.php">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./registrarMateria.html">Registrar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./materiaPrimaActualizacion.php">Actualizar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./materiaPrimaEliminar.php">Eliminar</a>
           </li>
           <li class="nav-item dropdown">
             <div class="dropdown" role="group">
@@ -54,34 +48,49 @@
       </div>
     </div>
   </nav>
-    <main class="container my-5 h-100">
-        <div class="d-flex flex-column flex-md-row justify-content-between">
-            <h4 class="text-md-start text-left">Materia prima</h4>
+  <main class="container my-5 h-100">
+    <div class="d-flex flex-column flex-md-row justify-content-between">
+      <h4 class="text-md-start text-left">Materia prima</h4>
+      <div class="actions-table">
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./registrarMateria.html">
+            Registrar <i class='bx bx-folder-plus icons-styles'></i></a>
         </div>
-        <hr>
-        <!-- Tabla de proveedores -->
-        <ul class="nav nav-tabs mb-4">
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="indexproveedores.php">Proveedores</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Materia prima</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="pedidos.php">Pedidos</a>
-            </li> 
-        </ul>
-        <div class="table-responsive">
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./materiaPrimaActualizacion.php">
+            Actualizar <i class='bx bx-edit icons-styles'></i></a>
+        </div>
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./materiaPrimaEliminar.php">
+            Eliminar <i class='bx bx-trash icons-styles'></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <!-- Tabla de proveedores -->
+    <ul class="nav nav-tabs mb-4">
+      <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="indexproveedores.php">Proveedores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Materia prima</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pedidos.php">Pedidos</a>
+      </li>
+    </ul>
+    <div class="table-responsive">
 
-            <?php
-            $conexion;
-            include_once "../../PHP/conexion_a_la_DB.php";
-            $sql = "SELECT id_materia_prima, color_materia, precio, cantidad_materia, descripcion_materia FROM materia_prima;";
-            $datos = $conexion->query($sql);
-            $result = mysqli_query($conexion, $sql);
-            
+      <?php
+      $conexion;
+      include_once "../../PHP/conexion_a_la_DB.php";
+      $sql = "SELECT id_materia_prima, color_materia, precio, cantidad_materia, descripcion_materia FROM materia_prima;";
+      $datos = $conexion->query($sql);
+      $result = mysqli_query($conexion, $sql);
 
-            echo '<table id="materia_prima" class="table">
+
+      echo '<table id="materia_prima" class="table">
             <thead>
                 <tr> 
                     <th scope="scope" >Id Materia Prima</th>
@@ -92,14 +101,14 @@
                 </tr>
             </thead>
             <tbody>';
-            if ($rta = $conexion->query($sql)) {
-                while ($row = $rta->fetch_assoc()) {
-                    $idMateriaPrima = $row["id_materia_prima"];
-                    $colorMateria = $row["color_materia"];
-                    $precio = $row["precio"];
-                    $cantidadMateria = $row["cantidad_materia"];
-                    $descripcionMateria = $row["descripcion_materia"];
-                    echo "
+      if ($rta = $conexion->query($sql)) {
+        while ($row = $rta->fetch_assoc()) {
+          $idMateriaPrima = $row["id_materia_prima"];
+          $colorMateria = $row["color_materia"];
+          $precio = $row["precio"];
+          $cantidadMateria = $row["cantidad_materia"];
+          $descripcionMateria = $row["descripcion_materia"];
+          echo "
             <tr>
                 <td>$idMateriaPrima</td>
                 <td>$colorMateria</td>
@@ -107,24 +116,27 @@
                 <td>$cantidadMateria</td>
                 <td>$descripcionMateria</td>
             </tr>";
-                }
-                echo "</tbody></table>\n";
-                $rta->free();
-            }
-            ?>
-        </div>
-        <hr class="my-5">
-    </main><br><br>
-    <br><br>
-    <footer>
-        <div class="copyright">
-            <div class="bd-container">
-                <p>💙 © 2024 Techlogistic. Todos los derechos reservados. 💚</p>
-                <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
-            </div>
-        </div>
-    </footer>
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        }
+        echo "</tbody></table>\n";
+        $rta->free();
+      }
+      ?>
+    </div>
+    <hr class="my-5">
+  </main><br><br>
+  <br><br>
+  <footer>
+    <div class="copyright">
+      <div class="bd-container">
+        <p>💙 © 2024 Techlogistic. Todos los derechos reservados. 💚</p>
+        <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a
+            href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
+      </div>
+    </div>
+  </footer>
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -136,7 +148,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script>
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('#materia_prima').DataTable({
       dom: 'Bfrtip',
       buttons: [
--- dashboard/proveedores/pedidos.php
+++ dashboard/proveedores/pedidos.php
@@ -11,19 +11,23 @@
     <!-- Box icons-->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- Estilos CSS -->
     <link rel="stylesheet" href="../../HTML/styles/techlogistic.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
 </head>
 
 <body class="vh-100">
     <!-- Header -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../IMAGES/favicon.png" alt="" class="navigation__image">Techlogistic</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img
+                    src="../../IMAGES/favicon.png" alt="" class="navigation__image">Techlogistic</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -31,20 +35,13 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="../../PHP/indexdash.php">Inicio</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./registrarPedido.php">Registrar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./pedidosActualizacion.php">Actualizar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./pedidosEliminar.php">Eliminar</a>
-                    </li>
                     <!-- Menu desplegable d-c flexon foto del  flex-columnusuario -->
                     <li class="nav-item dropdown">
                         <div class="dropdown" role="group">
-                            <a class="dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://higinnamora.github.io/techlogistic/images/profile/profile.png" alt="mdo" class="rounded-circle" width="38" height="38" />
+                            <a class="dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <img src="https://higinnamora.github.io/techlogistic/images/profile/profile.png"
+                                    alt="mdo" class="rounded-circle" width="38" height="38" />
                             </a>
                             <ul class="dropdown-menu dropdown-menu-lg-end">
                                 <!--<li><a class="dropdown-item" href="#">Mi perfil</a></li>-->
@@ -65,6 +62,20 @@
     <main class="container my-5 h-100">
         <div class="d-flex flex-column flex-md-row justify-content-between">
             <h4 class="text-md-start text-left">Pedidos</h4>
+            <div class="actions-table">
+                <div class="action-item">
+                    <a class="button margin-left" aria-current="page" href="./registrarPedido.php">Registrar<i
+                            class='bx bx-folder-plus icons-styles'></i></a>
+                </div>
+                <div class="action-item">
+                    <a class="button margin-left" aria-current="page" href="./pedidosActualizacion.php">Actualizar<i
+                            class='bx bx-edit icons-styles'></i></a>
+                </div>
+                <div class="action-item">
+                    <a class="button margin-left" aria-current="page" href="./pedidosEliminar.php">Eliminar<i
+                            class='bx bx-trash icons-styles'></i></a>
+                </div>
+            </div>
         </div>
         <hr>
         <!-- Tabla de proveedores -->
@@ -151,12 +162,15 @@
         <div class="copyright">
             <div class="bd-container">
                 <p>💙 © 2024 Techlogistic. Todos los derechos reservados. 💚</p>
-                <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
+                <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a
+                        href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
             </div>
         </div>
     </footer>
 
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -168,7 +182,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#pedidos').DataTable({
             dom: 'Bfrtip',
             buttons: [
