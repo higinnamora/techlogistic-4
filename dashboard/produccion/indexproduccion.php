8385ad04860a41fb7fe2ca048ddd8fba624c1373
Ginna Mora <higinnamora@gmail.com>
2024-06-17 11:47:31 -0500

    Techlogistic: Update of production  module actions.

--- dashboard/produccion/indexproduccion.php
+++ dashboard/produccion/indexproduccion.php
@@ -27,7 +27,8 @@ $datos = $conexion->query($sql);
   <!-- Box icons-->
   <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   <!-- Bootstrap 5 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
   <!-- Estilos CSS -->
   <link rel="stylesheet" href="../../HTML/styles/techlogistic.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
@@ -35,25 +36,18 @@ $datos = $conexion->query($sql);
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../IMAGES/favicon.png" alt="" class="navigation__image">Techlogistic</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <a href="../../PHP/indexdash.php" class="navbar-brand" title="Techlogistic"><img src="../../IMAGES/favicon.png"
+          alt="" class="navigation__image">Techlogistic</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto me-4 mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="../../PHP/indexdash.php">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./nuevoProducto.php">Registrar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./indexproduccionActualizacion.php">Actualizar</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="./indexproduccionEliminar.php">Eliminar</a>
           </li>
           <li class="nav-item dropdown">
             <div class="dropdown" role="group">
@@ -73,6 +67,20 @@ $datos = $conexion->query($sql);
   <main class="container my-5 h-100">
     <div class="d-flex flex-column flex-md-row justify-content-between gap-2">
       <h4 class="text-md-start text-left">Producción</h4>
+      <div class="actions-table">
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./nuevoProducto.php">Registrar<i
+              class='bx bx-folder-plus icons-styles'></i></a>
+        </div>
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./indexproduccionActualizacion.php">Actualizar<i
+              class='bx bx-edit icons-styles'></i></a>
+        </div>
+        <div class="action-item">
+          <a class="button margin-left" aria-current="page" href="./indexproduccionEliminar.php">Eliminar<i
+              class='bx bx-trash icons-styles'></i></a>
+        </div>
+      </div>
     </div>
     <hr>
     <!-- Tabla de Producción -->
@@ -117,13 +125,15 @@ $datos = $conexion->query($sql);
     <div class="copyright">
       <div class="bd-container">
         <p>💙 © 2024 Techlogistic. Todos los derechos reservados. 💚</p>
-        <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
+        <p><a href="../../HTML/terminos-y-condiciones.html">Términos y Condiciones</a> · <a
+            href="../../HTML/politica-de-privacidad.html">Política de Privacidad</a></p>
       </div>
     </div>
   </footer>
 </body>
 
-<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
@@ -133,7 +143,7 @@ $datos = $conexion->query($sql);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script>
-  $(document).ready(function() {
+  $(document).ready(function () {
     $('#productos').DataTable({
       dom: 'Bfrtip',
       buttons: [
