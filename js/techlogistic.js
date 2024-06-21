b6921e4510fe414e4ba46e9b9bba6bfd5d14f697
Ginna Mora <higinnamora@gmail.com>
2024-06-21 14:19:34 -0500

    Techlogistic: Scroll landing page & changes footer

--- js/techlogistic.js
+++ js/techlogistic.js
@@ -15,3 +15,28 @@ console.log("flkty", flkty);
 var flkty = new Flickity(".main-carousel", {
   // Elements options
 });
+
+// Mostrar desplazamiento superior
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // cuando el desplazamiento es superior a la altura de la ventana gráfica 560, agregue la clase show-scroll a la etiqueta scroll-top
+  if (this.scrollY >= 560) scrollTop.classList.add("scroll-top");
+  else scrollTop.classList.remove("scroll-top");
+}
+
+window.addEventListener("scroll", scrollTop);
+
+// Animación de revelación de desplazamiento
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "20px",
+  duration: 2000,
+  reset: null,
+});
+
+sr.reveal(
+  ".hero__data, .hero__img, .about__data, .features, .features-card, .banner, .big-card, .clients__card, .banner-chart, .contact, .copyright",
+  {
+    interval: 100,
+  }
+);
